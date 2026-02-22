59cb5fb271394ec88d077e4b4365f99812c008d1
Naveentn45 <naveen654k@gmail.com>
2026-02-22 13:47:34 +0530

    2C Progess

---
+++ Project-S-Collab/Assets/Naveen/Scripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private PlayerControl controls;
+    private Animator animator;
+    private float move;
+
+    void Awake()
+    {
+        controls = new PlayerControl();
+        animator = GetComponent<Animator>();
+
+        controls.Player.Move.performed += ctx => move = ctx.ReadValue<float>();
+        controls.Player.Move.canceled += ctx => move = 0f;
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(move, 0f, 0f);
+
+        CharacterController controller = GetComponent<CharacterController>();
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        // Flip character
+        if (move > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (move < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        animator.SetFloat("Speed", Mathf.Abs(move));
+    }
+}
